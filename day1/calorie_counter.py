3e2f6211f42593b73c147ba68e967086262de5a2
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-19 18:12:51 -0500

    finished day 1 part 1

--- day1/calorie_counter.py
+++ day1/calorie_counter.py
@@ -1,9 +1,10 @@
-
+# open the file and place all the lines into contents
 f = open('input.txt', 'r')
 contents = f.readlines()
 max_cal = 0
 sum = 0
 for line in contents:
+    # check if it's a blank line
     if line == "\n":
         if sum > max_cal:
             max_cal = sum
@@ -11,6 +12,7 @@ for line in contents:
         else:
             sum = 0
     else:
+        # cut off the '\n' from the line and convert it to an int
         sum += int(line[:-1])
 print(max_cal)
 
