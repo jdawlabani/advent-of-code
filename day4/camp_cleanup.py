2cf14249c110713b9bcbbbae774957897cc7e231
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-20 15:16:41 -0500

    optimized solution

--- day4/camp_cleanup.py
+++ day4/camp_cleanup.py
@@ -1,24 +1,9 @@
 import re
-# PART 1
-f = open('input.txt', 'r')
-contents = f.readlines()
-count = 0
-for line in contents:
-    # split line into array of the numbers and assign cast them into ints, then check if one range is contained in another
-    split = re.split(r'[-,\n]', line)
-    left_start = int(split[0])
-    left_end = int(split[1])
-    right_start = int(split[2])
-    right_end = int(split[3])
-    if((left_start <= right_start and left_end >= right_end) or (left_start >= right_start and left_end <= right_end)):
-        count += 1
-print(count)
-f.close()
 
-# PART 2
 f = open('input.txt', 'r')
 contents = f.readlines()
-count = 0
+part_1_count = 0
+part_2_count = 0
 for line in contents:
     # split line into array of the numbers and assign cast them into ints, then check if one range is contained in another
     split = re.split(r'[-,\n]', line)
@@ -26,7 +11,12 @@ for line in contents:
     left_end = int(split[1])
     right_start = int(split[2])
     right_end = int(split[3])
+    # part 1 check (one range is completely within the other range)
+    if((left_start <= right_start and left_end >= right_end) or (left_start >= right_start and left_end <= right_end)):
+        part_1_count += 1
+    #part 2 check (ANY overlap between the two ranges)
     if((left_start >= right_start and left_start <= right_end) or (left_end >= right_start and left_end <= right_end) or (right_start >= left_start and right_start <= left_end) or (right_end >= left_start and right_end <= left_end)):
-        count += 1
-print(count)
+        part_2_count += 1
+print("PART 1 answer: " + str(part_1_count))
+print("PART 2 answer: " + str(part_2_count))
 f.close()
