5cba32d8ccae38fff2aa95b50bef0e7827c73a90
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-20 14:34:30 -0500

    finished both parts

--- day3/rucksack-reorganization.py
+++ day3/rucksack-reorganization.py
@@ -25,6 +25,19 @@ print(priority_sum)
 f.close()
 
 # PART 2: finding common item in three word groups
+def find_common_item(group,sum):
+    common_item = list(set(group[0])&set(group[1])&set(group[2]))
+    for item in common_item:
+            try:
+                sum += (lowercase.index(item) + 1)
+            except:
+                pass
+            try:
+                sum += (uppercase.index(item) + 27)
+            except:
+                pass
+    return sum
+
 f = open('input.txt', 'r')
 contents = f.readlines()
 priority_sum = 0
@@ -36,16 +49,9 @@ for line in contents:
         count += 1
     else:
         count = 1
-        common_item = list(set(group[0])&set(group[1])&set(group[2]))
-        for item in common_item:
-            try:
-                priority_sum += (lowercase.index(item) + 1)
-            except:
-                pass
-            try:
-                priority_sum += (uppercase.index(item) + 27)
-            except:
-                pass
-        # set the first element of the next group
+        priority_sum = find_common_item(group, priority_sum)
+        group[0] = line[:-1]
+# run one last time for last group
+priority_sum = find_common_item(group, priority_sum)
 print(priority_sum)
 
