144fb2a070e6a32fb977e3b72be9fe8ec1d4ba67
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-21 12:42:54 -0500

    loading finished. Need to code solution for size

--- day7/no_space_left_on_device.py
+++ day7/no_space_left_on_device.py
@@ -5,25 +5,42 @@ class Directory:
         self.parent = parent
         self.name = name,
         self.children = {}
+        self.files = {}
+        # keep the size of the directory that way for deletion we only have to go as deep as the limit
+        self.size = 0
 
-current_dir = ''
 
+current_dir = Directory(None, 'root',{})
+# add the root to the directory
+current_dir.children.update({'/': Directory(current_dir,'/',{})})
+root_dir = current_dir.children['/']
 for i in f:
     # split the command into an array
     command = re.split(r'[ \n]', i)
     # pop off the last command which will be an empty string because of \n
     command.pop()
-
     match command[0]:
         case '$':
             match command[1]:
                 case 'cd':
                     match command[2]:
                         case '..':
-
+                            current_dir = current_dir.parent
                         case _:
+                            current_dir = current_dir.children[command[2]]
+                case 'ls':
+                    pass
+        case 'dir':
+            current_dir.children.update({command[1]: Directory(current_dir, command[1], {})})
+        case _:
+            current_dir.files.update({command[1]:command[0]})
+            # update the size of both the current directory and its parent directories
+            current_dir.size += int(command[0])
+            p = current_dir.parent
+            while p.parent != None:
+                p.size += int(command[0])
+                p = p.parent
 
 
-                    
 
 
