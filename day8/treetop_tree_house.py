45aad0e3a4a49298655509d20273bca703455fce
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-23 20:30:46 -0500

    save

--- day8/treetop_tree_house.py
+++ day8/treetop_tree_house.py
@@ -5,6 +5,7 @@ width = len(grid[0])
 length = len(grid)
 counted_trees = []
 count = 0
+max_score = 0
 
 # PART 1
 for i in range(width):
@@ -18,7 +19,9 @@ for i in range(width):
             counted = False
             for k in range(i-1,-1,-1):
                 if(int(grid[i][j]) <= int(grid[k][j])):
+                    t_score = i - k
                     visible = False
+
                     # print("false top: " + str(i) + " "+str(j))
                     break
             if visible and not counted:
@@ -26,43 +29,48 @@ for i in range(width):
                 count += 1
                 # counted_trees.append("top " + str(i) + " " + str(j))
             # check from bottom
-            if not visible:
-                visible = True
-                for k in range(i+1,length):
-                    if(int(grid[i][j]) <= int(grid[k][j])):
-                        visible = False
-                        # print("false bottom: " + str(i) + " "+str(j))
-                        break
-                if visible and not counted:
-                    counted = True
-                    count += 1
-                    # counted_trees.append("bottom " +str(i) + " " + str(j))
+            visible = True
+            for k in range(i+1,length):
+                if(int(grid[i][j]) <= int(grid[k][j])):
+                    visible = False
+                    b_score = k - i
+                    # print("false bottom: " + str(i) + " "+str(j))
+                    break
+            if visible and not counted:
+                counted = True
+                count += 1
+                # counted_trees.append("bottom " +str(i) + " " + str(j))
             
             # check from left
-            if not visible:
-                visible = True
-                for k in range(j-1,-1,-1):
-                    if(int(grid[i][j]) <= int(grid[i][k])):
-                        visible = False
-                        # print("false left: " + str(i) + " "+str(j))
-                        break
-                if visible and not counted:
-                    counted = True
-                    count += 1
-                    # counted_trees.append("left " + str(i) + " " + str(j))
+            visible = True
+            for k in range(j-1,-1,-1):
+                if(int(grid[i][j]) <= int(grid[i][k])):
+                    visible = False
+                    l_score = j-k
+                    # print("false left: " + str(i) + " "+str(j))
+                    break
+            if visible and not counted:
+                counted = True
+                count += 1
+                # counted_trees.append("left " + str(i) + " " + str(j))
 
-            if not visible:
-                visible = True
-                # check from right
-                for k in range(j+1,width):
-                    if(int(grid[i][j]) <= int(grid[i][k])):
-                        visible = False
-                        # print("false right: " + str(i) + " "+str(j))
-                        break
-                if visible and not counted:
-                    counted = True
-                    count += 1
-                    # counted_trees.append("right "+str(i) + " " + str(j))
+            visible = True
+            # check from right
+            for k in range(j+1,width):
+                if(int(grid[i][j]) <= int(grid[i][k])):
+                    visible = False
+                    r_score = k - j
+                    # print("false right: " + str(i) + " "+str(j))
+                    break
+            if visible and not counted:
+                counted = True
+                count += 1
+                # counted_trees.append("right "+str(i) + " " + str(j))
+            score = (l_score*t_score*b_score*r_score)
+            if(score > max_score):
+                max_score = score
+
 print(count)
+print(max_score)
 # print(counted_trees)
 
