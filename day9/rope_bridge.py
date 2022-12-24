9fd24cb2112f5eed34b102eb2d7c249e2e4ee13e
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-24 11:13:17 -0500

    237

--- day9/rope_bridge.py
+++ day9/rope_bridge.py
@@ -5,6 +5,10 @@ contents = f.read().strip().split('\n')
 tail_coordinates = ['0,0']
 # head_x, head_y, tail_x, tail_y coordinates
 coordinates = [0,0,0,0]
+# part 2 tail_coordinates and coordinates of all the knots
+tail_coordinates_2 = ['0,0']
+coordinates_2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+print(len(coordinates_2))
 
 
 def move(direction, steps, coordinates, t_coordinates):
@@ -42,12 +46,58 @@ def move(direction, steps, coordinates, t_coordinates):
                     if str(coordinates[2])+','+str(coordinates[3]) not in t_coordinates:
                         t_coordinates.append(str(coordinates[2])+','+str(coordinates[3]))
     return coordinates
+
+def move_2(direction, steps, coordinates, t_coordinates):
+    match direction:
+        case 'U':
+            for i in range(steps):
+                coordinates[1] -= 1
+                for j in range(1,len(coordinates)-2,2):
+                    if(abs(coordinates[j]- coordinates[j+2]) == 2):
+                        coordinates[j+1] = coordinates[j-1]
+                        coordinates[j+2] -= 1
+                        if str(coordinates[18])+','+str(coordinates[19]) not in t_coordinates:
+                            t_coordinates.append(str(coordinates[18])+','+str(coordinates[19]))
+        case 'D':
+            for i in range(steps):
+                coordinates[1] += 1
+                for j in range(1,len(coordinates)-2,2):
+                    if(abs(coordinates[j] - coordinates[j+2]) == 2):
+                        coordinates[j+1] = coordinates[j-1]
+                        coordinates[j+2] += 1
+                        if str(coordinates[18])+','+str(coordinates[19]) not in t_coordinates:
+                            t_coordinates.append(str(coordinates[18])+','+str(coordinates[19]))
+        case 'L':
+            for i in range(steps):
+                coordinates[0] -= 1
+                for j in range(0,len(coordinates)-2,2):
+                    if(abs(coordinates[j] - coordinates[j+2]) == 2):
+                        coordinates[j+3] = coordinates[j+1]
+                        coordinates[j+2] -= 1
+                        if str(coordinates[18])+','+str(coordinates[19]) not in t_coordinates:
+                            t_coordinates.append(str(coordinates[18])+','+str(coordinates[19]))
+        case 'R':
+            for i in range(steps):
+                coordinates[0] += 1
+                for j in range(0,len(coordinates)-2,2):
+                    if(abs(coordinates[j] - coordinates[j+2]) == 2):
+                        coordinates[j+3] = coordinates[j]
+                        coordinates[j+2] += 1
+                        if str(coordinates[18])+','+str(coordinates[19]) not in t_coordinates:
+                            t_coordinates.append(str(coordinates[18])+','+str(coordinates[19]))
+    return coordinates
+
+
 for command in contents:
     c = command.split(' ')
     coordinates = move(c[0], int(c[1]), coordinates, tail_coordinates)
 
+for command in contents:
+    c = command.split(' ')
+    coordinates = move_2(c[0], int(c[1]), coordinates_2, tail_coordinates_2)
 
 print("PART 1: " + str(len(tail_coordinates)))
+print("PART 2: " + str(len(tail_coordinates_2)))
 
 
 
