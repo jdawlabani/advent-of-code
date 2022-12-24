40d004a50f9073c16cee173ec1cc5c97c674a890
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-23 19:05:51 -0500

    save

--- day8/treetop_tree_house.py
+++ day8/treetop_tree_house.py
@@ -4,7 +4,14 @@ grid = f.read().strip().split('\n')
 counted_trees = []
 width = len(grid[0])
 length = len(grid)
+visible = 0
 
 for i in range(width):
     for j in range(length):
-        
+        # counts all the trees on the outside
+        if (i == 0 or i==width or j == 0 or j == length):
+            visible += 1
+            counted_trees.append(str(i) + ',' + str(j))
+        else:
+            
+
