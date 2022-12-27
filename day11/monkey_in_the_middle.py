0b9f3119b63979b6f87281141f2c182afc9ae686
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-27 15:54:25 -0500

    save

---
+++ day11/monkey_in_the_middle.py
@@ -0,0 +1,44 @@
+f = open('input.txt', 'r')
+contents = f.readlines()
+
+
+class Monkey:
+    def __init__(self, id, items, inspected, operation, test, to_monkey):
+        self.id = id
+        # worry level of the items each monkey has
+        self.items = items
+        # number of items inspected
+        self.inspected = inspected
+        # operation[0] is operator, operation[1] is either number or "old"
+        self.operation = operation
+        # will be a single number, since all tests are divisible by number
+        self.test = test
+        # to_monkey[0] if true, to_monkey[1] if flase
+        self.to_monkey = to_monkey
+
+monkeys = []
+count = 0
+
+for line in contents:
+    command = line.strip().split('\n')
+    # print(command)
+    if "Monkey" in command[0]:
+        id = command[0].split(' ')
+        id = id[1][0]
+        monkeys.append(Monkey(id,[],0,[],None,[]))
+    if "Starting items:" in command[0]:
+        print("items")
+    if "Operation:" in command[0]:
+        print("operation")
+    if "Test:" in command[0]:
+        print("test")
+    if "If true:" in command[0]:
+        print("true")
+    if "If false:" in command[0]:
+        print("false")
+
+
+    
+
+
+
