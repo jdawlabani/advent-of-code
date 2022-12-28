66e8139b4fc0b5831f130350e7bb7ae43b6b46e6
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-28 09:23:58 -0500

    save

--- day11/monkey_in_the_middle.py
+++ day11/monkey_in_the_middle.py
@@ -17,30 +17,40 @@ class Monkey:
         self.to_monkey = to_monkey
 
 monkeys = []
-count = 0
 
-for line in contents:
-    command = line.strip().split('\n')
-    # print(command)
-    if "Monkey" in command[0]:
-        id = command[0].split(' ')
-        id = id[1][0]
-        monkeys.append(Monkey(id,[],0,[],None,[]))
-    if "Starting items:" in command[0]:
-        items = command[0].split(' ')
-        for i in range(2,len(items)):
-            num = items[i][0:2]
-            monkeys[-1].items.append(int(num))
-        print(monkeys[-1].items)
-        print(items)
-    if "Operation:" in command[0]:
-        print("operation")
-    if "Test:" in command[0]:
-        print("test")
-    if "If true:" in command[0]:
-        print("true")
-    if "If false:" in command[0]:
-        print("false")
+def load(contents,monkeys):
+    for line in contents:
+        command = line.strip().split('\n')
+        # print(command)
+        if "Monkey" in command[0]:
+            id = command[0].split(' ')
+            id = id[1][0]
+            monkeys.append(Monkey(id,[],0,[],None,[]))
+        if "Starting items:" in command[0]:
+            items = command[0].split(' ')
+            for i in range(2,len(items)):
+                num = items[i][0:2]
+                monkeys[-1].items.append(int(num))
+            # print(monkeys[-1].items)
+            # print(items)
+        if "Operation:" in command[0]:
+            op = command[0].split(' ')
+            monkeys[-1].operation.append(op[-2])
+            monkeys[-1].operation.append(op[-1])
+            # print(monkeys[-1].operation)
+        if "Test:" in command[0]:
+            t = command[0].split(' ')
+            monkeys[-1].test = t[-1]
+            # print(monkeys[-1].test)
+        if "If true:" in command[0]:
+            t = command[0].split(' ')
+            monkeys[-1].to_monkey.append(t[-1])
+        if "If false:" in command[0]:
+            f = command[0].split(' ')
+            monkeys[-1].to_monkey.append(f[-1])
+
+
+load(contents, monkeys)
 
 
     
