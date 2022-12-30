7754490b9b68451fc61fa18661c7a334beaf69e3
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-12-30 12:10:01 -0500

    save

--- day13/distress_signal.py
+++ day13/distress_signal.py
@@ -1,7 +1,22 @@
 f = open('input.txt', 'r')
 g = open('ex1.txt', 'r')
 contents = g.read().split('\n')
-print(contents)
 
-for line in contents:
-    print()
+left = []
+right = []
+def read_input(input):
+    i = 0
+    for line in input:
+        if i == 0:
+            left.append(line)
+            i += 1
+        elif i == 1:
+            right.append(line)
+            i += 1
+        else:
+            i = 0
+    print(left)
+    print(right)
+
+def compare(left, right):
+    
